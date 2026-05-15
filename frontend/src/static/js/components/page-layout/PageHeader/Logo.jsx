b02957e9bdaae660852766b64b7286e7ea0306cd
starsers <yujieq0@gmail.com>
2026-05-15 16:48:32 +0800

    waic logo

--- frontend/src/static/js/components/page-layout/PageHeader/Logo.jsx
+++ frontend/src/static/js/components/page-layout/PageHeader/Logo.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-export const Logo = ({ src, loading = 'lazy', title, alt, href = '#' }) => {
-  return src ? (
+export const Logo = ({ loading = 'lazy', title, alt, href = '#' }) => {
+  return (
     <div className="logo">
       <a href={href} title={title}>
-        <span>
-          <img src={src} alt={alt || title} title={title} loading={loading} />
-        </span>
+        <div className="logo-wrapper" data-v-79554b65="">
+          <img
+            src="https://static.worldaic.com.cn/IMAGE2024/2025-02-18/0d86a1edd1e24a9e8068303bc555f683.png"
+            alt={alt || 'EY Logo'}
+            title={title}
+            loading={loading}
+            className="logo-image"
+            data-v-79554b65=""
+          />
+        </div>
       </a>
     </div>
-  ) : null;
+  );
 };
